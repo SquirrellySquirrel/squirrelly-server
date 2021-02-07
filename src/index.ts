9a49c4aef64c4be35b88f559a13935243eea4113
YuKitAs <moncher.merci@gmail.com>
2021-02-07 13:23:10 +0100

    Add eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { connectDB } from './database';
