9d8025877412b6178acf0d957467ed676f95196b
YuKitAs <moncher.merci@gmail.com>
2021-02-26 23:08:36 +0100

    Fix database clear

--- src/database.ts
+++ src/database.ts
@@ -12,12 +12,12 @@ const connection = {
     async clear() {
         const connection = getConnection();
         const entities = connection.entityMetadatas;
-        entities.forEach(async (entity) => {
+        for (const entity of entities) {
             const repository = connection.getRepository(entity.name);
             await repository.query('SET FOREIGN_KEY_CHECKS = 0;');
             await repository.query(`DELETE FROM ${entity.tableName}`);
             await repository.query('SET FOREIGN_KEY_CHECKS = 1;');
-        });
+        }
     },
 };
 
