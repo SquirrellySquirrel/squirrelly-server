5b30f14ee5463e7f08cb6e8fcd2048ae8fafee75
YuKitAs <moncher.merci@gmail.com>
2021-02-26 22:55:07 +0100

    Fix config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,7 +1,8 @@
 module.exports = {
+  name: "default",
   type: "mysql",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  port: parseInt(process.env.DB_PORT as string, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
