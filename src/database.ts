28825dc9101bcb1e5889ce05c0fc23bd786c34c2
YuKitAs <moncher.merci@gmail.com>
2021-03-04 21:07:23 +0100

    Escape table name using reserved word

--- src/database.ts
+++ src/database.ts
@@ -12,10 +12,11 @@ const connection = {
     async clear() {
         const connection = getConnection();
         const entities = connection.entityMetadatas;
+
         for (const entity of entities) {
             const repository = connection.getRepository(entity.name);
             await repository.query('SET FOREIGN_KEY_CHECKS = 0;');
-            await repository.query(`DELETE FROM ${entity.tableName}`);
+            await repository.query(`DELETE FROM \`${entity.tableName}\`;`);
             await repository.query('SET FOREIGN_KEY_CHECKS = 1;');
         }
     },
