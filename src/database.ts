1a1dfdcac32625a54018ee479246ef44817756b8
YuKitAs <moncher.merci@gmail.com>
2021-03-08 22:33:43 +0100

    Refactor db clear queries

--- src/database.ts
+++ src/database.ts
@@ -13,12 +13,12 @@ const connection = {
         const connection = getConnection();
         const entities = connection.entityMetadatas;
 
+        await connection.query('SET FOREIGN_KEY_CHECKS = 0;');
         for (const entity of entities) {
             const repository = connection.getRepository(entity.name);
-            await repository.query('SET FOREIGN_KEY_CHECKS = 0;');
-            await repository.query(`DELETE FROM \`${entity.tableName}\`;`);
-            await repository.query('SET FOREIGN_KEY_CHECKS = 1;');
+            await repository.query(`TRUNCATE TABLE \`${entity.tableName}\`;`);
         }
+        await connection.query('SET FOREIGN_KEY_CHECKS = 1;');
     },
 };
 
