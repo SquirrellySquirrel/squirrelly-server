ed4aaa838839608579eac07e3320be4aef90a614
YuKitAs <moncher.merci@gmail.com>
2021-06-13 03:52:09 +0200

    Remove device

--- src/repository/device.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Service } from 'typedi';
-import { EntityRepository, Repository } from "typeorm";
-import Device from "../entity/device";
-
-@Service()
-@EntityRepository(Device)
-export default class DeviceRepository extends Repository<Device> {
-}
