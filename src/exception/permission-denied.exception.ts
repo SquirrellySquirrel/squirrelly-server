86d95f2cd1fd6de8478b578bca8cac9cb0bd68ed
YuKitAs <moncher.merci@gmail.com>
2021-10-04 21:39:27 +0200

    Minor formatting

--- src/exception/permission-denied.exception.ts
+++ src/exception/permission-denied.exception.ts
@@ -2,6 +2,6 @@ import HttpException from './http.exception';
 
 export default class PermissionDeniedException extends HttpException {
     constructor(userId: string) {
-        super(403, 'Access not allowed for user: ' + userId);
+        super(403, `Access not allowed for user: ${userId}`);
     }
 }
