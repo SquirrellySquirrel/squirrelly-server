224603808ebb9a93943d90f392c833c910470b63
YuKitAs <moncher.merci@gmail.com>
2021-10-19 00:48:07 +0200

    Add return types explicitly

--- src/util/param-parser.ts
+++ src/util/param-parser.ts
@@ -1,11 +1,11 @@
-export function stringAsBoolean(param: string | undefined, defaultVal: boolean) {
+export function stringAsBoolean(param: string | undefined, defaultVal: boolean): boolean {
     if (param && param.trim().length > 0) {
         return JSON.parse(param);
     }
     return defaultVal;
 }
 
-export function stringAsNumber(param: string | undefined) {
+export function stringAsNumber(param: string | undefined): number | undefined {
     if (param && param.trim().length > 0) {
         return Number(param);
     }
