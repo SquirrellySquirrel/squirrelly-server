837160fce258617336f309ad5db5ea65645dfe80
YuKitAs <moncher.merci@gmail.com>
2021-12-05 15:25:08 +0100

    Verify entity existence before permission check, adjust exceptions

--- src/service/permission.service.ts
+++ src/service/permission.service.ts
@@ -2,9 +2,11 @@ import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import User, { UserRole } from '../entity/user';
 import PermissionDeniedException from '../exception/permission-denied.exception';
+import UnprocessableEntityException from '../exception/unprocessable-entity.exception';
 import CollectionRepository from '../repository/collection.repository';
 import CommentRepository from '../repository/comment.repository';
 import PostRepository from '../repository/post.repository';
+import UserService from './user.service';
 
 @Service()
 export default class PermissionService {
@@ -12,9 +14,13 @@ export default class PermissionService {
         @InjectRepository() private readonly postRepository: PostRepository,
         @InjectRepository() private readonly commentRepository: CommentRepository,
         @InjectRepository() private readonly collectionRepository: CollectionRepository,
+        private readonly userService: UserService,
     ) { }
 
     verifyUserAction(user: User, ownerId: string) {
+        this.verifyUser(user.id);
+        this.verifyUser(ownerId);
+
         if (user.role == UserRole.ADMIN) {
             return;
         }
@@ -25,10 +31,13 @@ export default class PermissionService {
     }
 
     async verifyPostAction(user: User, postId: string) {
+        this.verifyUser(user.id);
+
         const post = await this.postRepository.findOneWithCreator(postId);
         if (!post || !post.creator) {
-            throw new PermissionDeniedException(user.id);
+            throw new UnprocessableEntityException('Post does not exist or has no creator');
         }
+
         if (user.role == UserRole.ADMIN) {
             return;
         }
@@ -39,10 +48,13 @@ export default class PermissionService {
     }
 
     async verifyCollectionAction(user: User, collectionId: string) {
+        this.verifyUser(user.id);
+
         const collection = await this.collectionRepository.findOneWithCreator(collectionId);
         if (!collection || !collection.creator) {
-            throw new PermissionDeniedException(user.id);
+            throw new UnprocessableEntityException('Collection does not exist or has no creator');
         }
+
         if (user.role == UserRole.ADMIN) {
             return;
         }
@@ -53,15 +65,27 @@ export default class PermissionService {
     }
 
     async verifyCommentCreateAction(user: User, postId: string) {
-        const post = await this.postRepository.findOneWithCreator(postId);
-        if (!post || !post.public) {
+        this.verifyUser(user.id);
+
+        const post = await this.postRepository.findOne(postId);
+        if (!post) {
+            throw new UnprocessableEntityException('Post does not exist');
+        }
+
+        if (!post.public) {
             throw new PermissionDeniedException(user.id);
         }
     }
 
     async verifyCommentDeleteAction(user: User, commentId: string, postId: string) {
+        this.verifyUser(user.id);
+
         const post = await this.postRepository.findOneWithCreator(postId);
-        if (!post || !post.public || !post.creator) {
+        if (!post || !post.creator) {
+            throw new UnprocessableEntityException('Post does not exist or has no creator');
+        }
+
+        if (!post.public) {
             throw new PermissionDeniedException(user.id);
         }
 
@@ -80,4 +104,10 @@ export default class PermissionService {
         throw new PermissionDeniedException(user.id);
     }
 
+    private async verifyUser(userId: string) {
+        const user = await this.userService.getUserById(userId);
+        if (!user) {
+            throw new UnprocessableEntityException('User does not exist');
+        }
+    }
 }
